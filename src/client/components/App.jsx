0ddeac5c1e416c0140f96dbabc0f2d1decd2c96f
Jani Rapo <rapo.jani@gmail.com>
2018-04-09 19:53:35 +0300

    Added bootstrap style sheets to project

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import User from './User';
 import moment from 'moment';
+import { PageHeader } from 'react-bootstrap';
+
 // set locale to finnish
 moment.locale('fi');
+
+import 'bootstrap/dist/css/bootstrap.css';
 
 class App extends Component {
     render() {
         return (
             <div className="content">
+                <PageHeader>Puppy Chart</PageHeader>
                 <User />
             </div>
         );
