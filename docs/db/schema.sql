d1d10e68e43efc35ece669df559d531aa2a7f60d
Jani Rapo <rapo.jani@gmail.com>
2018-05-29 21:50:36 +0300

    updated schema updates to correct file

--- docs/db/schema.sql
+++ docs/db/schema.sql
@@ -1,8 +1,8 @@
+-- Dump of table users
 
-# Dump of table users
-# ------------------------------------------------------------
+DROP TABLE IF EXISTS `Users`;
 
-CREATE TABLE `users` (
+CREATE TABLE `Users` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `email` mediumtext COLLATE utf8_swedish_ci,
   `name` mediumtext COLLATE utf8_swedish_ci NOT NULL,
@@ -13,25 +13,11 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
 
-# Dump of table pets
-# ------------------------------------------------------------
+-- Dump of table Metrics
 
-CREATE TABLE `pets` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` mediumtext COLLATE utf8_swedish_ci NOT NULL,
-  `birth_date` date DEFAULT NULL,
-  `user_id` int(11) unsigned NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_user_id` (`user_id`),
-  CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
+DROP TABLE IF EXISTS `Metrics`;
 
-# Dump of table metrics
-# ------------------------------------------------------------
-
-CREATE TABLE `metrics` (
+CREATE TABLE `Metrics` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8_swedish_ci DEFAULT NULL,
   `unit` varchar(16) COLLATE utf8_swedish_ci DEFAULT NULL,
@@ -40,23 +26,47 @@ CREATE TABLE `metrics` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
 
-# Dump of table measurements
-# ------------------------------------------------------------
+INSERT INTO `Metrics` (`name`, `unit`, `created_at`, `updated_at`)
+VALUES	('weight','kg',NOW(),NULL);
 
-CREATE TABLE `measurements` (
+INSERT INTO `Metrics` (`name`, `unit`, `created_at`, `updated_at`)
+VALUES	('height','cm',NOW(),NULL);
+
+-- Dump of table Pets
+
+DROP TABLE IF EXISTS `Pets`;
+
+CREATE TABLE `Pets` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` mediumtext COLLATE utf8_swedish_ci NOT NULL,
+  `birth_date` date DEFAULT NULL,
+  `user_id` int(11) unsigned NOT NULL,
+  `active_flag` tinyint(1) NOT NULL DEFAULT '1',
+  `created_at` datetime NOT NULL DEFAULT NOW(),
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_user_id` (`user_id`),
+  CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `Users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
+
+-- Dump of table Measurements
+
+DROP TABLE IF EXISTS `Measurements`;
+
+CREATE TABLE `Measurements` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `metric_id` int(11) unsigned DEFAULT NULL,
   `value` float DEFAULT NULL,
   `measurement_dt` datetime DEFAULT NULL,
   `user_id` int(11) unsigned DEFAULT NULL,
-  `pet_id` int(11) unsigned DEFAULT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime DEFAULT NULL,
+  `pet_id` int(10) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `metric_id` (`metric_id`),
-  KEY `user_id` (`user_id`),
+  KEY `added_by_user_id` (`user_id`),
   KEY `pet_id` (`pet_id`),
-  CONSTRAINT `measurements_ibfk_1` FOREIGN KEY (`metric_id`) REFERENCES `metrics` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `measurements_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `measurements_ibfk_3` FOREIGN KEY (`pet_id`) REFERENCES `pets` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `measurements_ibfk_1` FOREIGN KEY (`metric_id`) REFERENCES `Metrics` (`id`),
+  CONSTRAINT `measurements_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `Users` (`id`),
+  CONSTRAINT `measurements_ibfk_3` FOREIGN KEY (`pet_id`) REFERENCES `Pets` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
